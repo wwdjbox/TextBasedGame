8c84e42696c23e0f93601a5d6fa3af453a64c7ad
wwdjbox <42366238+wwdjbox@users.noreply.github.com>
2018-09-05 11:49:40 +0800

    V1.1
    Hello

--- Text-Based-Game/Text-Based-Game/Program.cs
+++ Text-Based-Game/Text-Based-Game/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Text_Based_Game
 {
@@ -10,6 +11,8 @@ namespace Text_Based_Game
     {
         static void Main(string[] args)
         {
+            string[] filePaths = Directory.GetFiles(@"C:\Users\20111618\Documents\GitHub\TextBasedGame1\Text-Based-Game", "*.txt",
+                                         SearchOption.TopDirectoryOnly);
 
         }
     }
